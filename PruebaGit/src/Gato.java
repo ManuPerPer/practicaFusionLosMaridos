e807575985e81ee45972218df5975bdb3acd1bf1
'Manuel <manuel.perez-perez@iessampedro.org>
2022-04-05 16:27:13 +0200

    creo clase Gato y creo un gato en principal

---
+++ PruebaGit/src/Gato.java
@@ -0,0 +1,24 @@
+
+public class Gato extends Mascota {
+
+	public Gato(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Soy un gato, me llamo " + getNombre() + ", y hago miau";
+	}
+	
+	
+	
+
+}
--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -4,7 +4,10 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		
+		Gato gato0= new Gato("Michi");
 		
+		
+		System.out.println(gato0);
 		
 
 		
