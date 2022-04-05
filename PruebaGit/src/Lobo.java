d2d663d1a676a690f2220c98cd7811b817e809c1
'jesus' <jesusjlg@hotmail.com>
2022-04-05 16:27:21 +0200

    He terminado de crear mi clase lobo y crearlop en la principal

---
+++ PruebaGit/src/Lobo.java
@@ -0,0 +1,18 @@
+
+public class Lobo extends Mascota{
+
+	public Lobo(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Soy un lobo y me llamo " + getNombre() + ", y hago AUUUUUUUUUUUUU!" ;
+	}
+
+	
+	
+	
+}
--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -3,7 +3,9 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		
+		Lobo elpedazodelobo=new Lobo("Jesus");
 		
+		System.out.println(elpedazodelobo);
 		
 		
 
