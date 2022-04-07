b5faaf577040a3915497897c7fb240166aaec5f7
'Manuel <manuel.perez-perez@iessampedro.org>
2022-04-07 18:06:36 +0200

    he añadido el atributo pelaje a la clase Lobo

--- PruebaGit/src/Lobo.java
+++ PruebaGit/src/Lobo.java
@@ -1,15 +1,20 @@
 
 public class Lobo extends Mascota{
-
+	private String pelaje= "Gris";
 	public Lobo(String nombre) {
 		super(nombre);
+		
+		
+		
 		// TODO Auto-generated constructor stub
+		
+		
 		
 	}
 
 	@Override
 	public String toString() {
-		return "Soy un lobo y me llamo " + getNombre() + ", y hago AUUUUUUUUUUUUU!" ;
+		return "Soy un lobo y me llamo " + getNombre() + ", y hago AUUUUUUUUUUUUU! y tengo el pelo de color" + pelaje;
 	}
 
 	
