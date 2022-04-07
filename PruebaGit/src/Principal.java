13ed7fec5bb246d826923a5499c8ddd1d1df9364
'jesus' <jesusjlg@hotmail.com>
2022-04-07 17:55:35 +0200

    he fusionado por completo las ramas

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -5,11 +5,11 @@ public class Principal {
 		
 		Lobo elpedazodelobo=new Lobo("Jesus");
 		
-<<<<<<< HEAD
+
 		System.out.println(elpedazodelobo);
-=======
+
 		Gato gato0= new Gato("Michi");
->>>>>>> refs/remotes/origin/rama.Manu
+
 		
 		
 		System.out.println(gato0);
