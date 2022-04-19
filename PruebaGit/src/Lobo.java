202a5f242ea8caf10aca17d037a01f1f60919849
'Manuel <manuel.perez-perez@iessampedro.org>
2022-04-19 16:07:45 +0200

    modificacion del mandarmensaje

--- PruebaGit/src/Lobo.java
+++ PruebaGit/src/Lobo.java
@@ -1,17 +1,31 @@
 
 public class Lobo extends Mascota{
+	private String pelaje;
+	
+	public String getPelaje() {
+		return pelaje;
+	}
+
+	public void setPelaje(String pelaje) {
+		this.pelaje = pelaje;
+	}
 
 	public Lobo(String nombre) {
 		super(nombre);
+		
+		
+		
 		// TODO Auto-generated constructor stub
+		
+		
 		
 	}
 
 	@Override
 	public String toString() {
-		return "Soy un lobo y me llamo " + getNombre() + ", y hago AUUUUUUUUUUUUU!" ;
+		return "Hola equipo! Soy un lobo y me llamo " + getNombre() + " y te voy a contar algo o no.";
 	}
-
+	
 	
 	
 	
