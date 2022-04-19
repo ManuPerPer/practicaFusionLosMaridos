2b1dace290ff1f244c6e146f28add58b87d360fb
'Manuel <manuel.perez-perez@iessampedro.org>
2022-04-19 15:51:13 +0200

    elimino gato1

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -11,12 +11,12 @@ public class Principal {
 		Gato gato0= new Gato("Michi");
 
 
-		Gato gato1= new Gato("Garfield");
+		
 
 		
 		
 		System.out.println(gato0);
-		System.out.println(gato1);
+	
 
 
 		
